ca47fdc640bc7a5efb0445c7748ee6699632d799
bmeyer0127 <bmeyer0127@gmail.com>
2023-05-27 11:54:25 -0400

    Fixed formatting and added breakpoints
    Co-authored-by: Kaleb Baldwin <baldwinjkaleb@gmail.com>

--- src/Components/CharacterCard.jsx
+++ src/Components/CharacterCard.jsx
@@ -1,29 +1,36 @@
-const CARD_STYLES =
-  "card d-flex flex-row justify-content-between p-2 align-items-center";
-
 function CharacterCard(props) {
   return (
-    <div className={CARD_STYLES}>
-      <ion-icon size="large" name="accessibility-outline"></ion-icon>
-      <h2 className="m-0">{props.name}</h2>
-      <table>
-        <tr>
-          <td>Gender: </td>
-          <td className="ps-2">{props.gender}</td>
-        </tr>
-        <tr>
-          <td>Height: </td>
-          <td className="ps-2">{props.height}</td>
-        </tr>
-        <tr>
-          <td>Weight: </td>
-          <td className="ps-2">{props.weight}</td>
-        </tr>
-        <tr>
-          <td>Birth Year: </td>
-          <td className="ps-2">{props.birthYear}</td>
-        </tr>
-      </table>
+    <div className="card p-2 m-2">
+      <div className="row align-items-center">
+        <div className="col-auto">
+          <ion-icon size="large" name="accessibility-outline"></ion-icon>
+        </div>
+        <div className="col-auto">
+          <h2 className="m-0">{props.name}</h2>
+        </div>
+        <div className="col-auto">
+          <table>
+            <tbody>
+              <tr>
+                <td>Gender: </td>
+                <td className="ps-2">{props.gender}</td>
+              </tr>
+              <tr>
+                <td>Height: </td>
+                <td className="ps-2">{props.height}</td>
+              </tr>
+              <tr>
+                <td>Weight: </td>
+                <td className="ps-2">{props.weight}</td>
+              </tr>
+              <tr>
+                <td>Birth Year: </td>
+                <td className="ps-2">{props.birthYear}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
